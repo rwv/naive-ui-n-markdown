0acc8997f0303ec7388cce4d41f38aef834c29df
Seedgou <i@zczc.cz>
2023-10-27 15:10:46 +0800

    add syntax highlight

--- src/App.vue
+++ src/App.vue
@@ -3,25 +3,31 @@ import { NCode, NGrid, NGi } from "naive-ui";
 import NMarkdown from "./components/NMarkdown.vue";
 import SampleMD from "./sample.md?raw";
 import Markdown from "./components/Markdown.vue";
+import hljs from "highlight.js/lib/core";
+import javascript from "highlight.js/lib/languages/javascript";
+import { NConfigProvider } from "naive-ui";
+hljs.registerLanguage("javascript", javascript);
 
 const md = SampleMD;
 </script>
 
 <template>
   <div style="padding: 5em">
-    <header>Naive UI NMarkdown</header>
-    <main>
-      <n-grid x-gap="30" :cols="3">
-        <n-gi>
-          <n-markdown :md="md" />
-        </n-gi>
-        <n-gi>
-          <Markdown :md="md" />
-        </n-gi>
-        <n-gi>
-          <n-code :code="md" />
-        </n-gi>
-      </n-grid>
-    </main>
+    <n-config-provider :hljs="hljs">
+      <header>Naive UI NMarkdown</header>
+      <main>
+        <n-grid x-gap="30" :cols="3">
+          <n-gi>
+            <n-markdown :md="md" />
+          </n-gi>
+          <n-gi>
+            <Markdown :md="md" />
+          </n-gi>
+          <n-gi>
+            <n-code :code="md" />
+          </n-gi>
+        </n-grid>
+      </main>
+    </n-config-provider>
   </div>
 </template>
